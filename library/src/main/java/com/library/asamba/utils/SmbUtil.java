02da8afe7ad02655cff441ba48422eea812aef8c
shaojun <wusj2017@gmail.com>
2017-06-19 23:07:18 +0800

    add smb test

---
+++ library/src/main/java/com/library/asamba/utils/SmbUtil.java
@@ -0,0 +1,15 @@
+package com.library.asamba.utils;
+
+/**
+ * <pre>
+ *     author : blueflybee
+ *     e-mail : wusj_2017@163.com
+ *     time   : 2017/03/07
+ *     desc   : samba工具类
+ *     version: 1.0
+ * </pre>
+ */
+
+public class SmbUtil {
+
+}
--- library/src/test/java/com/library/asamba/ExampleUnitTest.java
+++ library/src/test/java/com/library/asamba/ExampleUnitTest.java
@@ -2,6 +2,12 @@ package com.library.asamba;
 
 import org.junit.Test;
 
+import java.io.IOException;
+import java.net.MalformedURLException;
+
+import jcifs.smb.SmbException;
+import jcifs.smb.SmbFile;
+
 import static org.junit.Assert.*;
 
 /**
@@ -10,8 +16,90 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
+
+    private static final String SMB_URL_PWD = "smb://shaojun:123456@192.168.1.102/homes/";
+    private static final String SMB_URL = "smb://192.168.1.102/homes/";
+
     @Test
-    public void addition_isCorrect() throws Exception {
-        assertEquals(4, 2 + 2);
+    public void listString() {
+
+        SmbFile file = null;
+        try {
+            file = new SmbFile(SMB_URL);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        try {
+            file.connect();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        long t1 = System.currentTimeMillis();
+        String[] files = new String[0];
+        try {
+            files = file.list();
+        } catch (SmbException e) {
+            e.printStackTrace();
+        }
+        long t2 = System.currentTimeMillis() - t1;
+
+        for (int i = 0; i < files.length; i++) {
+            System.out.println(" " + files[i]);
+        }
+        System.out.println();
+        System.out.println(files.length + " files in " + t2 + "ms");
     }
+
+    @Test
+    public void testConnect() {
+
+        SmbFile file = null;
+        try {
+            file = new SmbFile(SMB_URL_PWD);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        try {
+            file.connect();
+            System.out.println(file.canRead());
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @Test
+    public void listFiles()  {
+
+        int a = 0;
+        SmbFile file = null;
+        SmbFile[] files = new SmbFile[0];
+
+        try {
+            file = new SmbFile(SMB_URL_PWD);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
+        long t1 = System.currentTimeMillis();
+
+        try {
+            files = file.listFiles();
+        } catch (SmbException e) {
+            e.printStackTrace();
+        }
+
+        long t2 = System.currentTimeMillis() - t1;
+
+        for (int i = 0; i < files.length; i++) {
+            System.out.print(" " + files[i].getName());
+        }
+        System.out.println();
+        System.out.println(files.length + " files in " + t2 + "ms");
+    }
+
+
 }
